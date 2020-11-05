914bedb3219bc6e844cd27f683c9f71ce694f442
Marcio Campos <marciomrbcampos@gmail.com>
2020-11-05 18:00:48 -0300

    head consertado

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,7 @@ import "aos/dist/aos.css";
 import Header from '../components/header';
 
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
     AOS.init();
   }, []);
@@ -20,19 +20,19 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="author" content="Marcio Campos">
-        <meta name="description" content="Curriculum Marcio Campos - Desginer Gráfico e Web Designer, Moro em Tietê-SP. Aqui você pode ver todas as habilidades e experiências que tive!">
-        <meta name="keywords" content="HTML,CSS,XML,JavaScript">
-        <meta property="og:image" content="/capa.jpg">
-        <meta property="og:image:type" content="image/jpeg">
-        <meta property="og:image:width" content="800">
-        <meta property="og:image:height" content="480">
-        <meta property="og:locale" content="pt_BR">
-        <meta property="og:url" content="https://marciocampos.com.br">
-        <meta property="og:title" content="Marcio Campos | Currículo">
-        <meta property="og:site_name" content="Marcio Campos">
-        <meta property="og:type" content="website">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="Marcio Campos" />
+        <meta name="description" content="Curriculum Marcio Campos - Desginer Gráfico e Web Designer, Moro em Tietê-SP. Aqui você pode ver todas as habilidades e experiências que tive!" />
+        <meta name="keywords" content="HTML,CSS,XML,JavaScript" />
+        <meta property="og:image" content="/capa.jpg" />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="800" />
+        <meta property="og:image:height" content="480" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:url" content="https://marciocampos.com.br" />
+        <meta property="og:title" content="Marcio Campos | Currículo" />
+        <meta property="og:site_name" content="Marcio Campos" />
+        <meta property="og:type" content="website" />
       </Head>
         <Header />
         <Component {...pageProps} />
