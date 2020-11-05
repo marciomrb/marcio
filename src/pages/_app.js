3e51f9bdca8716654ae0df94be5e40739a259e0b
Marcio Campos <marciomrbcampos@gmail.com>
2020-11-05 17:55:15 -0300

    head

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import 'animate.css';
 import AOS from "aos";
 import Head from 'next/head';
 
+
 import "aos/dist/aos.css";
 
 
@@ -20,6 +21,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Marcio Campos | Currículo</title>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
       </Head>
       <Header />
       <Component {...pageProps} />
